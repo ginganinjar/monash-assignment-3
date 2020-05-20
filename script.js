6ed86b93d48ca88d7944d9c1bda2883cbbd43b19
David Sparrius <david@mstone.com.au>
2020-05-20 12:53:37 +1000

    workingcode

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-  alert(passwordText);
-}
-
-
 function getRandomNumber(totalNumber) {
   randomResponse = Math.floor(Math.random() * totalNumber);
     return randomResponse;
@@ -31,18 +23,26 @@ function adjustLength() {
         var alphabetChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
         var numbersChar = ['0','1','2','3','4','5','6','7','8','9'];
      
-      
+
+do {
        
       // lets set up a random scenario of 1 to 4. based on that let's choose the ideal response. For 0, special, 1 alpha, 2 caps, 3 lowercase
       letterType = getRandomNumber(4);
 
+      // use this to ensure that only the correct type is returned
+      var satisfied = false;
+      
+ 
+    
+        //alert("im in this loop");
 
       // special characters
       if (letterType === 0) {
         // check if switch is on..
         if (document.getElementById("SpecialSwitch").checked == true) {    
             letterType = specialChar[getRandomNumber(specialChar.length)];              
-              }
+              satisfied = true;
+            }
           }
 
           // capitals
@@ -50,7 +50,8 @@ function adjustLength() {
             // check if switch is on..
             if (document.getElementById("capitals").checked == true) {    
                 letterType = alphabetChar[getRandomNumber(alphabetChar.length)];              
-                  }
+                satisfied = true;    
+                }
               }
 
           // lowercase
@@ -59,7 +60,8 @@ function adjustLength() {
             if (document.getElementById("lowercaseSwitch").checked == true) {    
                 letterType = alphabetChar[getRandomNumber(alphabetChar.length)];
                 letterType = letterType.toLowerCase();
-                  }
+                satisfied = true;
+                }
               }
 
               // numerical
@@ -67,8 +69,11 @@ function adjustLength() {
                 // check if switch is on..
                 if (document.getElementById("numericSwitch").checked == true) {    
                     letterType = numbersChar[getRandomNumber(numbersChar.length)];
+                    satisfied = true;
                     }
                   }
+              } while (satisfied === false);
+
           
             return letterType;
 }
@@ -79,13 +84,21 @@ function adjustLength() {
         passwordSize = document.getElementById("passwordlength").value;
           thePassWordFinal = "";
 
+          // check that at least one condition is turned to on
+          if ((document.getElementById("numericSwitch").checked == false) && (document.getElementById("lowercaseSwitch").checked == false) && (document.getElementById("capitals").checked == false) && (document.getElementById("SpecialSwitch").checked == false)) {
+              alert ("You must select the type of characterset you'd like your password to include from the toggle switches below");
+              return;
+
+          }
+
         
           for (loopCounter = 0; loopCounter < passwordSize; loopCounter++) {
             //go fetch the desired letter;
             thePassWordFinal = thePassWordFinal + returnCharacter();
 
           }
-               //alert(thePassWordFinal );
+             
+              // insert the password
                document.getElementById("password").innerHTML = thePassWordFinal;
       }
 
