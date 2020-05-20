3834039ef2fc53825129dc07a61706afd43a788d
David Sparrius <david@mstone.com.au>
2020-05-20 11:57:48 +1000

    password_creator

--- script.js
+++ script.js
@@ -9,16 +9,85 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
   alert(passwordText);
-
 }
+
+
+function getRandomNumber(totalNumber) {
+  randomResponse = Math.floor(Math.random() * totalNumber);
+    return randomResponse;
+}
+
 
 // get value of password length
 function adjustLength() {
   var passwordLengthVar = document.getElementById("passwordlength").value;
-  //alert(passwordLengthVar);
-  document.getElementById("PasswordLengthLabel").innerHTML = passwordLengthVar + " characters";
+   document.getElementById("PasswordLengthLabel").innerHTML = passwordLengthVar + " characters";
 
   }
+
+    function returnCharacter() {
+        var specialChar = ['@', '#', '$', '%', '<', '^', '<'];
+        // Will just use an array and switch between caps and lowercase depending on switch
+        var alphabetChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+        var numbersChar = ['0','1','2','3','4','5','6','7','8','9'];
+     
+      
+       
+      // lets set up a random scenario of 1 to 4. based on that let's choose the ideal response. For 0, special, 1 alpha, 2 caps, 3 lowercase
+      letterType = getRandomNumber(4);
+
+
+      // special characters
+      if (letterType === 0) {
+        // check if switch is on..
+        if (document.getElementById("SpecialSwitch").checked == true) {    
+            letterType = specialChar[getRandomNumber(specialChar.length)];              
+              }
+          }
+
+          // capitals
+          if (letterType === 1) {
+            // check if switch is on..
+            if (document.getElementById("capitals").checked == true) {    
+                letterType = alphabetChar[getRandomNumber(alphabetChar.length)];              
+                  }
+              }
+
+          // lowercase
+          if (letterType === 2) {
+            // check if switch is on..
+            if (document.getElementById("lowercaseSwitch").checked == true) {    
+                letterType = alphabetChar[getRandomNumber(alphabetChar.length)];
+                letterType = letterType.toLowerCase();
+                  }
+              }
+
+              // numerical
+              if (letterType === 3) {
+                // check if switch is on..
+                if (document.getElementById("numericSwitch").checked == true) {    
+                    letterType = numbersChar[getRandomNumber(numbersChar.length)];
+                    }
+                  }
+          
+            return letterType;
+}
+
+
+    function createPassword() {
+      // get the password size preference and loop until it is satisfied  
+        passwordSize = document.getElementById("passwordlength").value;
+          thePassWordFinal = "";
+
+        
+          for (loopCounter = 0; loopCounter < passwordSize; loopCounter++) {
+            //go fetch the desired letter;
+            thePassWordFinal = thePassWordFinal + returnCharacter();
+
+          }
+               //alert(thePassWordFinal );
+               document.getElementById("password").innerHTML = thePassWordFinal;
+      }
 
 
   // adjust toggle switch based on capitals on or off
@@ -37,16 +106,10 @@ function adjustLength() {
           
 
 // Add event listener to generate button
-    generateBtn.addEventListener("click", writePassword);
+    generateBtn.addEventListener("click", createPassword);
     passwordlength.addEventListener("change", adjustLength);
 
     SpecialStatusSwitch.addEventListener("change",function(){toggleStatus("SpecialSwitch", "specialcharactersLabel","Special Characters  ")}, false);
     capitalsOn.addEventListener("change",function(){toggleStatus("capitals", "CapitalsStatus","Capital Letters  ")}, false);
     lowercaseSwitch.addEventListener("change",function(){toggleStatus("lowercaseSwitch", "lowercaseStatus","Lowercase  ")}, false);
     numericSwitch.addEventListener("change",function(){toggleStatus("numericSwitch", "numericStatusLabel","Numerical ")}, false);
-
-
-  //   someObj.addEventListener("click", function(){
-  //     some_function(someVar);
-  // }, false);
-
